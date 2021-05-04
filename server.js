5d98881a203d0fb1aa0bd2b494ab8f930da65227
Aiki <57651898+aikiDenver@users.noreply.github.com>
2021-05-04 15:55:08 -0600

    Add files via upload

--- server.js
+++ server.js
@@ -35,25 +35,6 @@ app.use(passport.initialize());
 
 var router = express.Router();
 
-/*mongoose.Promise = global.Promise;
-const uri = process.env.DB;
-
-mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true}).
-catch(err => console.log(err));
-
-console.log("connected to mongo atlas (users)");
-*/
-// function getJSONObjectForMovie(req) {
-//     var json = {
-//         body: "No body"
-//     };
-
-//     if (req.body != null) {
-//         json.body = req.body;
-//     }
-
-//     return json;
-// }
 
 router.post('/signup', function(req, res) {
     if (!req.body.username || !req.body.password) {
@@ -71,7 +52,7 @@ router.post('/signup', function(req, res) {
                     return res.send({ success: false, message: 'A user with that username already exists.'});
                 else
                     console.log(err);
-                    return;
+                return;
             }
 
             res.json({success: true, msg: 'Successfully created new user.'})
@@ -83,79 +64,79 @@ router.post('/signin', function (req, res) {
     var userNew = new User();
     userNew.username = req.body.username;
     userNew.password = req.body.password;
-    
+
     function handleResponse(json) {
-    console.log(json);
+        console.log(json);
     }
-    
+
     function getClientIp(req) {
-    var ip = req.headers['x-forwarded-for'] ? req.headers['x-forwarded-for'].split(',')[0] : req.connection.remoteAddress;
-    return ip;
+        var ip = req.headers['x-forwarded-for'] ? req.headers['x-forwarded-for'].split(',')[0] : req.connection.remoteAddress;
+        return ip;
     }
-    
+
     var geolocationParams = new GeolocationParams();
     geolocationParams.setIPAddress(getClientIp(req));
-    
-    
-    
-     console.log(userNew);
-    
-     User.findOne({ username: userNew.username }).select('name username password').exec(function(err, user) {
-         if (err) {
-             console.log(err);
-             res.send(err);
-         }
-    
-         user.comparePassword(userNew.password, function(isMatch) {
-             if (isMatch) {
-                 var userToken = { id: user.id, username: user.username };
-                 var token = jwt.sign(userToken, process.env.SECRET_KEY);
-    
-                 User.findOneAndUpdate({username: req.body.username}, {countryCode: iipgeolocationApi.getGeolocation(handleResponse, geolocationParams).country_code2
-                 }, function(err, user) {
-                     if(err){
-                         res.status(403).json({success:false, message: "Could not update ip"});
-                     }else{
-                         res.status(200).json({success: true, message: "Updated ip"});
-                     }
-                 })
-    
-                 res.json({success: true, token: 'JWT ' + token});
-    
-                 }
-             else {
-                 res.status(401).send({success: false, msg: 'Authentication failed.'});
-             }
-         })
-     })
+
+
+
+    console.log(userNew);
+
+    User.findOne({ username: userNew.username }).select('name username password').exec(function(err, user) {
+        if (err) {
+            console.log(err);
+            res.send(err);
+        }
+
+        user.comparePassword(userNew.password, function(isMatch) {
+            if (isMatch) {
+                var userToken = { id: user.id, username: user.username };
+                var token = jwt.sign(userToken, process.env.SECRET_KEY);
+
+                User.findOneAndUpdate({username: req.body.username}, {countryCode: ipgeolocationApi.getGeolocation(handleResponse, geolocationParams).country_code2
+                }, function(err, user) {
+                    if(err){
+                        res.status(403).json({success:false, message: "Could not update ip"});
+                    }else{
+                        res.status(200).json({success: true, message: "Updated ip"});
+                    }
+                })
+
+                res.json({success: true, token: 'JWT ' + token});
+
+            }
+            else {
+                res.status(401).send({success: false, msg: 'Authentication failed.'});
+            }
+        })
+    })
 });
 
 //get and post for both items and transaction
 router.route('/transaction')
-        .post(authJwtController.isAuthenticated, function (req, res) {
-            // post function
-            var transaction = new Transaction();
+    .post(authJwtController.isAuthenticated, function (req, res) {
+        // post function
+        var transaction = new Transaction();
 
-            transaction.id = req.body.id;
-            transaction.cart = req.body.cart;
-            transaction.date = req.body.date;
-            transaction.ip = req.body.ip;
+        transaction.id = req.body.id;
+        transaction.cart = req.body.cart;
+        transaction.date = req.body.date;
+        transaction.ip = req.body.ip;
 
-            transaction.save(function (err) {
-                if (err) {
-                    return res.json(err);
-                }
-                res.json({success: true, msg: 'Transaction saved.'});
-            })
-
+        transaction.save(function (err) {
+            if (err) {
+                return res.json(err);
+            }
+            res.json({success: true, msg: 'Transaction saved.'});
         })
 
+    })
 
-        .get(authJwtController.isAuthenticated, function (req, res) {
-            Transaction.find({}, function(err, transaction) {
-                res.json({Transaction: transaction});
-            })
-        });
+
+    .get(authJwtController.isAuthenticated, function (req, res) {
+        Transaction.find({}, function(err, transaction) {
+            res.json({Transaction: transaction});
+        })
+    });
 
 
 router.route('/item')
@@ -217,7 +198,7 @@ router.route('/item')
 //         var movieMap = {};
 
 //         movies.forEach(function(movie) {
-//             movieMap[movie._id] = movie; 
+//             movieMap[movie._id] = movie;
 //         })
 
 //         res.json({success: true, movies: movieMap});
@@ -262,7 +243,7 @@ router.route('/item')
 //             res.send(err);
 //             console.log(err);
 //         }
-        
+
 //         res.json({success:true, movieupdated: movie});
 //     });
 
@@ -275,7 +256,7 @@ router.route('/item')
 //             res.send(err);
 //             console.log(err);
 //         }
-        
+
 //         res.json({success: true, message: "movie deleted"});
 //     });
 
@@ -332,7 +313,7 @@ router.route('/item')
 //                     reviews: reviewMap
 //                 });
 //             })
-            
+
 //         })
 
 //     } else {
@@ -385,13 +366,13 @@ router.route('/item')
 //             console.log(err);
 //             res.send(err);
 //         }
-        
+
 //         // make sure mov is not null aka it exists
 //         if (mov === null) {
 //             res.json({success: false, msg: "couldn't find movie, check that movie name is correct"})
 //             return; // throw error, prevent node from continuing on with save, etc.
 //         }
-        
+
 //         // if movie is in database, save review for this movie
 //         review.save(function (err) {
 //             if (err) {
@@ -411,5 +392,3 @@ router.route('/item')
 app.use('/', router);
 app.listen(process.env.PORT || 8080);
 module.exports = app; // for testing only
-
-
